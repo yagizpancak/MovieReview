55699a730d244657cae8bbbc1923d7a764478d79
Yağız Pançak <yagizcan.pancak@somera.com.tr>
2022-01-21 16:55:39 +0300

    Initial commit

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $connection = mysqli_connect('localhost', 'root', '');
+    $connection = mysqli_connect('localhost', 'root', '') or die('There was a problem connecting to the database');
     $db = mysqli_select_db($connection, 'movie_review');
 ?>
