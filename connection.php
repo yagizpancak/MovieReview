aeed128ae40f6ac6bd44decc02dc8bc52821abd3
Yağız Pançak <yagizcan.pancak@somera.com.tr>
2022-01-21 17:41:56 +0300

    Database

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 
-//<?php
+
   //  $connection = mysqli_connect('localhost', 'root', '') or die('There was a problem connecting to the database');
     //$db = mysqli_select_db($connection, 'movie_review') or die('There was a problem connecting to the database');
-//?>
+
 
 <?php
     //Get Heroku ClearDB connection information
